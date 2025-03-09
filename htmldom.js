c4ceb0891b28fea7665c3da5ea037640f9c42031
Long Phung <longsydn1105@gmail.com>
2025-03-10 00:11:19 +0700

    upadate htmldom.js

---
+++ htmldom.js
@@ -0,0 +1,37 @@
+// HTML DOM: Document Object Model
+/**
+ * 1. Element: ID, Class, Tag, CSS Selector, HTML Collection
+ * 2. Attribute
+ * 3. Text
+ */
+
+// ------------------------------------
+
+// API: Application Programming Interface
+
+// JAVA SCRIPT: Browser || Server (NodeJS)
+
+// Browser: HTML, CSS, JS, HTML DOM -> DOM -> WEB API
+// Server: NodeJS, Express, MongoDB
+
+//console.log(document);
+//document.write('Hello');
+
+// var headingNode = document.getElementById('heading'); 
+// var headingsNode2 = document.getElementsByClassName('heading2');
+//var headingsNode3 = document.getElementsByTagName('h2');
+
+// console.log(headingsNode2);
+// console.log(headingNode);
+//console.log(headingsNode3);
+
+//var headingNode = document.querySelector('.box .heading-2:last-of-type');
+// Có thể dùng first-child hoặc last-child để lấy phần tử đầu tiên hoặc cuối cùng 
+// hoặc nth-child(index) index là vị trí muốn lấy để lấy phần tử đầu tiên hoặc cuối cùng
+//console.log(headingNode);
+
+// var headingNodes = document.querySelectorAll('.box .heading-2');
+// console.log(headingNodes[1]);
+
+console.log(document.forms['form-1']);
+console.log(document.forms[1]);
