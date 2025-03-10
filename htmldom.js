4a89ef42d325f7566543284be8bfcfc7dd81f39a
Long Phung <longsydn1105@gmail.com>
2025-03-10 22:37:05 +0700

    update html dom and insert index.html

--- htmldom.js
+++ htmldom.js
@@ -33,5 +33,24 @@
 // var headingNodes = document.querySelectorAll('.box .heading-2');
 // console.log(headingNodes[1]);
 
-console.log(document.forms['form-1']);
-console.log(document.forms[1]);
+// console.log(document.forms['form-1']);
+// console.log(document.forms[1]);
+
+// var boxNode = document.querySelector('.box-1');
+//Công việc 1: Sử dụng tới boxNode
+
+// Công việc 2: Sử dụng các li elements bên trong .box-1
+
+// console.log(boxNode.querySelectorAll('li'));
+
+//---------------ÔN TẬP----------------------
+
+//1. getElementById
+//2. getElementsByClassName
+//3. getElementsByTagName
+//4. querySelector
+//5. querySelectorAll
+
+//6. HTML Collection
+
+//7. document.write
